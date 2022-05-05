664a5bbd09ffcead27f61dde6bf61921df60a2fd
Jimkelly Percine <jimkellypercine@jimkellys-mbp.dyn.brandeis.edu>
2022-05-05 15:30:03 -0400

    changes made

---
+++ models/Animedata.js
@@ -0,0 +1,18 @@
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+const Mixed = Schema.Types.Mixed;
+
+
+var AnimeSchema = Schema( {
+    studio: String,
+    genres: [String],
+    hype: Number,
+    description: String,
+    title: [String],
+    start_date: Mixed,
+ } );
+ 
+
+module.exports = mongoose.model( 'Animedata', AnimeSchema );
